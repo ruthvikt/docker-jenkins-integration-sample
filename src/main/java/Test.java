29a21812e22ae6bebeac753d034dc20da6704b2c
ruthvikt <ruthvikreddy.t.com>
2021-12-18 17:26:07 +0530

    adding Dockerfile

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello this is JEnkins git intigration ");
+        System.out.println("Hello this is JEnkins git docker intigration ");
     }
 }
